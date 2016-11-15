16ea2d844bdba49c0a43225514579c70d693750e
Roger Silva <orogersilva@gmail.com>
2016-11-15 16:47:15 -0200

    Adding screenshot capture on instrumentation tests.

--- app/src/androidTest/java/com/orogersilva/bmimanager/calcinfo/UC_CalcOfBmi.java
+++ app/src/androidTest/java/com/orogersilva/bmimanager/calcinfo/UC_CalcOfBmi.java
@@ -1,10 +1,14 @@
 package com.orogersilva.bmimanager.calcinfo;
 
+import android.app.Activity;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import com.orogersilva.bmimanager.R;
+import com.squareup.spoon.Spoon;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -12,8 +16,10 @@ import org.junit.runner.RunWith;
 
 import java.text.NumberFormat;
 import java.text.ParseException;
+import java.util.Collection;
 import java.util.Locale;
 
+import static android.support.test.espresso.Espresso.getIdlingResources;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -23,6 +29,7 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.runner.lifecycle.Stage.RESUMED;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -60,16 +67,53 @@ public class UC_CalcOfBmi {
 
         // ACT
 
+        Spoon.screenshot(mActivityTestRule.getActivity(), "initial_state");
+
         onView(withId(R.id.weightEditText)).perform(typeText(WEIGHT), pressImeActionButton());
         onView(withId(R.id.meterEditText)).perform(typeText(METERS_HEIGHT), pressImeActionButton());
         onView(withId(R.id.centimeterEditText)).perform(typeText(CENTIMETERS_HEIGHT), closeSoftKeyboard());
+
+        Spoon.screenshot(mActivityTestRule.getActivity(), "after_typing_input_data");
 
         onView(withId(R.id.calculateBmiButton)).perform(scrollTo(), click());
 
         // ASSERT
 
+        Spoon.screenshot(getActivityInstance(), "bmi_result");
+
         onView(withId(R.id.bmiResultMessageTextView)).check(matches(withText(EXPECTED_BMI_MESSAGE)));
         onView(withId(R.id.diagnosisMessageTextView)).check(matches(withText(EXPECTED_DIAGNOSIS_MESSAGE)));
+    }
+
+    // endregion
+
+    // region UTILITY METHODS
+
+    /**
+     * Get current Activity shown on the screen.
+     * @return Current Activity resumed.
+     */
+    public Activity getActivityInstance() {
+
+        final Activity[] activity = new Activity[1];
+
+        InstrumentationRegistry.getInstrumentation().runOnMainSync(new Runnable( ) {
+
+            public void run() {
+
+                Activity currentActivity;
+
+                Collection resumedActivities = ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(RESUMED);
+
+                if (resumedActivities.iterator().hasNext()){
+
+                    currentActivity = (Activity) resumedActivities.iterator().next();
+                    activity[0] = currentActivity;
+                }
+            }
+        });
+
+        return activity[0];
     }
 
     // endregion
